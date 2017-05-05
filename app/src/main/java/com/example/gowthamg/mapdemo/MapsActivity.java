4b1dc16d86d8df62fb97bbf026e027118c439ef8
Gowtham Genupur <gowtham.gourov@gmail.com>
2017-05-05 10:31:47 +0530

    Map and Listview On the same screen

--- app/src/main/java/com/example/gowthamg/mapdemo/MapsActivity.java
+++ app/src/main/java/com/example/gowthamg/mapdemo/MapsActivity.java
@@ -1,7 +1,12 @@
 package com.example.gowthamg.mapdemo;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -9,6 +14,8 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -22,6 +29,25 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        //List<String> l = Arrays.asList(h);
+
+        ListView myListView = (ListView) findViewById(R.id.list);
+    final ArrayList<String> cricketers = new ArrayList<String>();
+//        cricketers.addAll(l);
+       cricketers.add("Dhoni");
+       cricketers.add("Dravid");
+      cricketers.add("yuvi");
+
+
+
+        ArrayAdapter<String> arrayadapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,cricketers);
+        myListView.setAdapter(arrayadapter);
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MapsActivity.this,"Hello "+cricketers.get(i), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 //hellooo
 
